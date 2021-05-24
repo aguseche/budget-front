d26b916b7ef628fc707e8ebf2a99de319859da6d
Agustin Etcheverry <etxeagus@gmail.com>
2021-05-24 11:11:40 -0300

    eslint

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@ export default {
 };
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap");
 #app {
-  font-family: 'Libre Baskerville', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Libre Baskerville", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
